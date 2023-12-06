1080905a78b90a3f086534f8972828588fe0fca3
mayhim182 <mayankatnitrkl@gmail.com>
2023-12-06 12:20:15 +0530

    daily code push

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -29,16 +29,26 @@ function skip(){
 
 
 function handleRangeUpdate(){
-    console.log(this);
+    // console.log('%c%s','color:orange;background-color:pink',this, this.name);
+    video[this.name] = this.value;
+}
+
+function handleProgress(){
+    const percent = (video.currentTime/video.duration)*100;
+    progressBar.style.flexBasis = `${percent}`;
 }
 
 // Hook up listeners
 video.addEventListener('click',togglePlay);
 video.addEventListener('play',updateButton);
 video.addEventListener('pause',updateButton);
+video.addEventListener('timeupdate',handleProgress);
 
 toggle.addEventListener('click',togglePlay);
 
 skipButtons.forEach(button => {
     button.addEventListener('click',skip);
 })
+
+ranges.forEach(range=>range.addEventListener('change',handleRangeUpdate));
+ranges.forEach(range=>range.addEventListener('mousemove',handleRangeUpdate));
